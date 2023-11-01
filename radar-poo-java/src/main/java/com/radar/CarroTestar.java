a8769a2409f873c2a704a26d2875e525bcc07ae7
Flitzso <106411702+flitzso@users.noreply.github.com>
2023-11-01 14:01:00 -0300

    Update CarroTestar.java

--- radar-poo-java/src/main/java/com/radar/CarroTestar.java
+++ radar-poo-java/src/main/java/com/radar/CarroTestar.java
@@ -4,10 +4,8 @@ public class CarroTestar {
 
     public static void main(String[] args) {
 
-        // Instanciando o objeto:
         Carro c1 = new Carro();
 
-        // Declarando valores aos atributos:
         c1.nome = "Uno";
         c1.marca = "Fiat";
         c1.placa = "0000-AAA";
@@ -15,10 +13,8 @@ public class CarroTestar {
         c1.vel = 60;
         c1.radar = "";
 
-        // Chamando os metodos
-        c1.acelerar(60);
-        c1.frear(50);
+        c1.acelerar(0);
         c1.radar();
-        System.out.println("Model: " + c1.nome + "\nMarca: " + c1.marca + "\nPlaca: " + c1.placa + "\nAno: " + c1.ano);
+        System.out.println("Modelo: " + c1.nome + "\nMarca: " + c1.marca + "\nPlaca: " + c1.placa + "\nAno: " + c1.ano);
     }
 }
